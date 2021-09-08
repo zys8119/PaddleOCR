0cd0b010f2208ad8f49de3a4b108e95bcede4c88
Ahsen Khaliq <ahsen.khaliq@gmail.com>
2021-09-08 14:59:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ from paddleocr import PaddleOCR,draw_ocr
 from PIL import Image
 import gradio as gr
 
-# Paddleocr supports Chinese, English, French, German, Korean and Japanese.
-# You can set the parameter `lang` as `ch`, `en`, `fr`, `german`, `korean`, `japan`
-# to switch the language model in order.
 ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
 def inference(img):
     img_path = img.name
     result = ocr.ocr(img_path, cls=True)
     for line in result:
         print(line)
-
+    
     # draw result
     image = Image.open(img_path).convert('RGB')
     boxes = [line[0] for line in result]
@@ -32,5 +29,4 @@ gr.Interface(
     gr.outputs.Image(type="file", label="Output"),
     title=title,
     description=description,
-    article=article    
-    ).launch(debug=True)
+    article=article).launch(debug=True)
