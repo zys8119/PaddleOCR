daf81213bcf3b8112117358a7da14b194a006e46
Zhang Jun <jzhang533@gmail.com>
2025-04-22 17:23:36 +0800

    udpate demo
    Signed-off-by: Zhang Jun <jzhang533@gmail.com>

--- README.md
+++ README.md
@@ -4,31 +4,9 @@ emoji: ⚡
 colorFrom: pink
 colorTo: green
 sdk: gradio
+sdk_version: 5.23.1
 app_file: app.py
 pinned: false
-sdk_version: 5.23.1
+license: apache-2.0
+short_description: Extract text from images in multiple languages
 ---
-
-# Configuration
-
-`title`: _string_  
-Display title for the Space
-
-`emoji`: _string_  
-Space emoji (emoji-only character allowed)
-
-`colorFrom`: _string_  
-Color for Thumbnail gradient (red, yellow, green, blue, indigo, purple, pink, gray)
-
-`colorTo`: _string_  
-Color for Thumbnail gradient (red, yellow, green, blue, indigo, purple, pink, gray)
-
-`sdk`: _string_  
-Can be either `gradio` or `streamlit`
-
-`app_file`: _string_  
-Path to your main application file (which contains either `gradio` or `streamlit` Python code).  
-Path is relative to the root of the repository.
-
-`pinned`: _boolean_  
-Whether the Space stays on top of your list.
--- app.py
+++ app.py
@@ -1,69 +1,49 @@
-import os
-os.system('pip install paddlepaddle')
-os.system('pip install paddleocr')
-import requests
 from paddleocr import PaddleOCR, draw_ocr
 from PIL import Image
 import gradio as gr
 
-ocr = PaddleOCR(use_angle_cls=True, lang=lang,use_gpu=False)
-
-def download_image(url, save_path):
-    """
-    Download an image from a specified URL and save it to the specified path
-    
-    Args:
-        url (str): URL of the image
-        save_path (str): Path to save the image
-        
-    Returns:
-        bool: True if download is successful, False otherwise
-    """
-    try:
-        response = requests.get(url, stream=True)
-        if response.status_code == 200:
-            with open(save_path, 'wb') as file:
-                for chunk in response.iter_content(chunk_size=8192):
-                    file.write(chunk)
-            print(f"Image successfully downloaded and saved as: {save_path}")
-            return True
-        else:
-            print(f"Download failed, status code: {response.status_code}")
-            return False
-    except Exception as e:
-        print(f"Error occurred during download: {str(e)}")
-        return False
-
-# Download example image from GitHub
-image_url = "https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/v2.8.0/doc/imgs_en/img_12.jpg"
-download_image(image_url, "example.jpg")
+lang_list = ['ch', 'en', 'fr', 'german', 'korean', 'japan']
+ocr_dict = {lang: PaddleOCR(lang=lang, use_angle_cls=True, use_gpu=False) for lang in lang_list}
 
 def inference(img, lang):
-	
-	img_path = img  
-	result = ocr.ocr(img_path, cls=True)[0]
-	image = Image.open(img_path).convert('RGB')
-	boxes = [line[0] for line in result]
-	txts = [line[1][0] for line in result]
-	scores = [line[1][1] for line in result]
-	im_show = draw_ocr(image, boxes, txts, scores,
-			   font_path='./simfang.ttf')
-	im_show = Image.fromarray(im_show)
-	im_show.save('result.jpg')
-	return 'result.jpg'
+    ocr = ocr_dict[lang]
+
+    img_path = img
+    result = ocr.ocr(img_path, cls=True)[0]
+    image = Image.open(img_path).convert('RGB')
+    boxes = [line[0] for line in result]
+    txts = [line[1][0] for line in result]
+    scores = [line[1][1] for line in result]
+    im_show = draw_ocr(image, boxes, txts, scores,
+                       font_path='./simfang.ttf')
+    im_show = Image.fromarray(im_show)
+    im_show.save('result.jpg')
+    return 'result.jpg'
+
 
 title = 'PaddleOCR'
-description = 'Gradio demo for PaddleOCR. PaddleOCR demo supports Chinese, English, French, German, Korean and Japanese. To use it, simply upload your image and choose a language from the dropdown menu, or click one of the examples to load them. Read more at the links below.'
-article = "<p style='text-align: center'>Awesome multilingual OCR toolkits based on PaddlePaddle <a href='https://github.com/PaddlePaddle/PaddleOCR'>Github Repo</a></p>"
-examples = [['example.jpg','en']]
+description = '''
+- Gradio demo for PaddleOCR. PaddleOCR demo supports Chinese, English, French, German, Korean and Japanese. 
+- To use it, simply upload your image and choose a language from the dropdown menu, or click one of the examples to load them. Read more at the links below.
+- [Docs](https://paddlepaddle.github.io/PaddleOCR/), [Github Repository](https://github.com/PaddlePaddle/PaddleOCR).
+'''
+
+examples = [
+    ['en_example.jpg','en'],
+    ['cn_example.jpg','ch'],
+    ['jp_example.jpg','japan'],
+]
+
 css = ".output_image, .input_image {height: 40rem !important; width: 100% !important;}"
 gr.Interface(
     inference,
-    [gr.Image(type='filepath', label='Input'), gr.Dropdown(choices=['ch', 'en', 'fr', 'german', 'korean', 'japan'], value='en', label='language')],
+    [
+        gr.Image(type='filepath', label='Input'),
+        gr.Dropdown(choices=lang_list, value='en', label='language')
+    ],
     gr.Image(type='filepath', label='Output'),
     title=title,
     description=description,
-    article=article,
     examples=examples,
     css=css
-    ).launch(debug=True)
+    ).launch(debug=False)
